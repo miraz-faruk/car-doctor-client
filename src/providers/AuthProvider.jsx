27bbcc8dde0d935377a969c977996b1c021f6b03
mirazfaruk <mirazfaruk@gmail.com>
2024-12-05 10:26:09 +0600

    Secure client side with credentials

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -2,6 +2,7 @@ import { createContext, useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from "../firebase/firebase.config";
 import PropTypes from "prop-types";
+import axios from "axios";
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
@@ -27,9 +28,24 @@ const AuthProvider = ({ children }) => {
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            const userEmail = currentUser?.email || user?.email;
+            const loggedUser = { email: userEmail };
             setUser(currentUser);
             console.log("current user", currentUser)
             setLoading(false);
+            // If user exist then issue a token
+            if (currentUser) {
+                axios.post('http://localhost:5001/jwt', loggedUser, { withCredentials: true })
+                    .then(res => {
+                        console.log('token response', res.data)
+                    });
+            }
+            else {
+                axios.post('http://localhost:5001/logout', loggedUser, { withCredentials: true })
+                .then(res => {
+                    console.log(res.data);
+                })
+            }
         }, []);
         return () => unsubscribe();
     }, []);
